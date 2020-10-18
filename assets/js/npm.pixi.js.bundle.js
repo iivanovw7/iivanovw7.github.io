d397a367b429c26c639128675e2580a9c974e656
Igor <iivanovw7@gmail.com>
2020-10-18 13:32:17 +0300

    new realistaion

--- assets/js/npm.pixi.js.bundle.js
+++
@@ -1,8 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{19:function(r,n,i){"use strict";i(437);var t=i(107),e=i(108),a=i(109),u=i(110),g=i(0);i.d(n,"k",(function(){return g}));var c=i(82);i.d(n,"b",(function(){return c.a}));var s=i(4);i.d(n,"i",(function(){return s.p}));var o=i(38),d=i(111),l=i(112),f=i(113);i.d(n,"j",(function(){return f.a}));var b=i(114),P=i(15),p=i(192),v=i(143),m=i(193),w=i(194),h=i(195),k=i(196),J=(i(441),i(442),i(443),i(1),i(8));i.d(n,"c",(function(){return J.b}));var j=i(81);i.d(n,"d",(function(){return j.a}));var x=i(3);i.d(n,"e",(function(){return x.g}));i(21),i(188),i(24);var y=i(42);i.d(n,"f",(function(){return y.a}));var S=i(189);i.d(n,"a",(function(){return S.a}));var q=i(34);i.d(n,"g",(function(){return q.b})),i.d(n,"h",(function(){return q.d}));i(5);
-/*!
- * pixi.js - v5.2.0
- * Compiled Wed, 06 Nov 2019 02:32:43 UTC
- *
- * pixi.js is licensed under the MIT License.
- * http://www.opensource.org/licenses/mit-license
- */s.m.registerPlugin("accessibility",t.a),s.m.registerPlugin("extract",e.a),s.m.registerPlugin("interaction",a.a),s.m.registerPlugin("particle",d.a),s.m.registerPlugin("prepare",u.a),s.m.registerPlugin("batch",s.d),s.m.registerPlugin("tilingSprite",f.b),o.b.registerPlugin(b.a),o.b.registerPlugin(l.a),c.a.registerPlugin(P.b),c.a.registerPlugin(o.a);p.a,v.a,v.b,m.a,w.a,h.a,k.a}}]);
