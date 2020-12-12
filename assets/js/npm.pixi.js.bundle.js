3eae50247c84b263615861b2f31090018b8b6652
Igor <iivanovw7@gmail.com>
2020-12-13 00:23:05 +0300

    test build

--- assets/js/npm.pixi.js.bundle.js
+++
@@ -1,8 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{20:function(r,n,i){"use strict";i(438);var t=i(108),e=i(109),a=i(110),u=i(111),g=i(0);i.d(n,"k",(function(){return g}));var c=i(83);i.d(n,"b",(function(){return c.a}));var s=i(4);i.d(n,"i",(function(){return s.p}));var o=i(40),d=i(112),l=i(113),f=i(114);i.d(n,"j",(function(){return f.a}));var b=i(115),P=i(15),p=i(193),v=i(144),m=i(194),w=i(195),h=i(196),k=i(197),J=(i(442),i(443),i(444),i(1),i(8));i.d(n,"c",(function(){return J.b}));var j=i(82);i.d(n,"d",(function(){return j.a}));var x=i(3);i.d(n,"e",(function(){return x.g}));i(22),i(189),i(25);var y=i(44);i.d(n,"f",(function(){return y.a}));var S=i(190);i.d(n,"a",(function(){return S.a}));var q=i(36);i.d(n,"g",(function(){return q.b})),i.d(n,"h",(function(){return q.d}));i(5);
-/*!
- * pixi.js - v5.2.0
- * Compiled Wed, 06 Nov 2019 02:32:43 UTC
- *
- * pixi.js is licensed under the MIT License.
- * http://www.opensource.org/licenses/mit-license
- */s.m.registerPlugin("accessibility",t.a),s.m.registerPlugin("extract",e.a),s.m.registerPlugin("interaction",a.a),s.m.registerPlugin("particle",d.a),s.m.registerPlugin("prepare",u.a),s.m.registerPlugin("batch",s.d),s.m.registerPlugin("tilingSprite",f.b),o.b.registerPlugin(b.a),o.b.registerPlugin(l.a),c.a.registerPlugin(P.b),c.a.registerPlugin(o.a);p.a,v.a,v.b,m.a,w.a,h.a,k.a}}]);
